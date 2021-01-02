d3fac1924b7b33007f358f8d6aa8b075c252495c
valentino320 <valentino320@gmail.com>
2021-01-02 20:46:39 +0000

    Edit functuin added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,34 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
-
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description, :info))
+    if @article.save
+      flash[:notice] = "Article was created succesfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description, :info))
+      flash[:notice] = "Article was updated successfuly."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
 end
