d57d4f2d4789fd42d5f775c90cf8a9b9e1a1482d
valentino320 <valentino320@gmail.com>
2021-01-03 16:28:59 +0000

    all good to go

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
-    #byebug
-    @article = Article.find(params[:id])
+    
   end
 
   def index
@@ -13,11 +15,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description, :info))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created succesfully."
       redirect_to @article
@@ -27,13 +29,29 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description, :info))
+    
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfuly."
       redirect_to @article
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description, :info)
   end
 
 end
