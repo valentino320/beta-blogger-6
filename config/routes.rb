81f24f91398d766053ce445d2e6b0954822b092d
valentino320 <valentino320@gmail.com>
2021-01-04 15:02:18 +0000

    Updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   
+  get 'home', to: 'pages#home'
   get "about", to: "pages#about"
   resources :articles
 end
