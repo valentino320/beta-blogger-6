0960c22c76c88f139c881af4409b4352e13ab30d
valentino320 <valentino320@gmail.com>
2021-01-05 12:59:17 +0000

    user signup step 1

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get 'home', to: 'pages#home'
   get "about", to: "pages#about"
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_193658) do
+ActiveRecord::Schema.define(version: 2021_01_05_113627) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_01_04_193658) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
