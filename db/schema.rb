9a90b4d4f0e6927c7d35a0b5106976dc287d3780
valentino320 <valentino320@gmail.com>
2021-01-08 11:04:35 +0000

    added category tsts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_191205) do
+ActiveRecord::Schema.define(version: 2021_01_08_103020) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2021_01_07_191205) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
