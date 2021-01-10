dfb4f16f194ce05d66b106d4f35190782ae0b830
valentino320 <valentino320@gmail.com>
2021-01-10 16:12:38 +0000

    removed byebug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,6 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    byebug
     @article = Article.new(article_params)
     @article.user = current_user
     if @article.save
